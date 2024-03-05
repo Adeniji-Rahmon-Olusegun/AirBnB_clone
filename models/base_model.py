a1b98b0cba354cb37fc95eb9f1e2bf440fcdf85f
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-03-05 14:03:20 -0800

    models and tests directories added

---
+++ models/base_model.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module contains base model for this AirBnB clone project"""
+
+
+class BaseModel:
+    """This is the parent class for all other classes in this project"""
+    pass
