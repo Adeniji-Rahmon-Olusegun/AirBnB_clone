170cd77b5e3940ab474b22e7b0f85a3aeb2e8aba
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-03-10 11:09:55 -0700

    base_model.py updated

--- models/base_model.py
+++ models/base_model.py
@@ -12,8 +12,8 @@ class BaseModel:
         """Initializes the class with this attributes"""
 
         self.id = str(uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns the string representation of instances"""
@@ -23,14 +23,14 @@ class BaseModel:
     def save(self):
         """Updates the updated_at with the current datetime"""
 
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dict. containing keys/values of __dict__ wrt instance"""
 
         dicts = self.__dict__
-        # dicts["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f"
-        # dicts["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f"
+        dicts["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        dicts["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         dicts["__class__"] = self.__class__.__name__
 
         return dicts
