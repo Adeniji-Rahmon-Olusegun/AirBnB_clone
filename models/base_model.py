c03bd12d573788e0ce99a6fc6cde81d8141eb15e
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-03-11 03:29:45 -0700

    base_model.py modified

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """Returns the string representation of the instances"""
@@ -26,7 +26,7 @@ class BaseModel:
         with the current datetime
         """
 
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of
