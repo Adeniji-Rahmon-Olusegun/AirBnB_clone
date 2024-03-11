024c73226c8a6c5000b588106804e014fdea87be
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-03-11 06:37:09 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,8 +12,8 @@ class BaseModel:
         """Initializes the class with this attributes"""
 
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        self.created_at = datetime.utcnow()
+        self.updated_at = self.created_at
 
     def __str__(self):
         """Returns the string representation of the instances"""
@@ -27,15 +27,15 @@ class BaseModel:
         with the current datetime
         """
 
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of
         __dict__ of the instance
         """
         keys_values = self.__dict__.copy()
-        keys_values["created_at"] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        keys_values["updated_at"] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
         keys_values["__class__"] = self.__class__.__name__
+        keys_values["created_at"] = self.created_at.isoformat()
+        keys_values["updated_at"] = self.updated_at.isoformat()
 
         return keys_values
