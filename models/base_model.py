c82a954bd6ca51a004486e6b7d445b3b3eac2516
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-03-11 03:08:18 -0700

    base_model.py updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module contains base model for this AirBnB clone project"""
 
-from uuid import uuid4
+from uuid
 from models import storage
 from datetime import datetime
 
@@ -9,43 +9,32 @@ from datetime import datetime
 class BaseModel:
     """This is the parent class for all other classes in this project"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """Initializes the class with this attributes"""
 
-        time_4mat = "%Y-%m-%dT%H:%M:%S.%f"
-
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ["created_at", "updated_at"]:
-                        value = datetime.strptime(value, time_4mat)
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-        storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """Returns the string representation of instances"""
+        """Returns the string representation of the instances"""
 
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates the updated_at with the current datetime"""
+        """updates public instance attribute updated_at
+        with the current datetime
+        """
 
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
-        """Returns a dict. containing keys/values of __dict__ wrt instance"""
+        """Returns a dictionary containing all keys/values of
+        __dict__ of the instance
+        """
+        keys_values = self.__dict__.copy()
+        keys_values["created_at"] = self.created_at.isoformat()
+        keys_values["updated_at"] = self.updated_at.isoformat()
+        keys_values["__class__"] = self.__class__.__name__
 
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        dicts = self.__dict__.copy()
-        for key, value in dicts.items():
-            if isinstance(value, datetime):
-                dicts[key] = value.strftime(time_format)
-        dicts["__class__"] = self.__class__.__name__
-
-        return dicts
+        return keys_values
