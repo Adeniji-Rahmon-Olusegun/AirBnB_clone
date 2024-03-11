b154242fa8687192add43cccf66bee4ad937bcc7
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-03-11 05:34:05 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -25,15 +25,15 @@ class BaseModel:
         with the current datetime
         """
 
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of
         __dict__ of the instance
         """
         keys_values = self.__dict__.copy()
-        keys_values["created_at"] = self.created_at.isoformat()
-        keys_values["updated_at"] = self.updated_at.isoformat()
+        keys_values["created_at"] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        keys_values["updated_at"] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
         keys_values["__class__"] = self.__class__.__name__
 
         return keys_values
