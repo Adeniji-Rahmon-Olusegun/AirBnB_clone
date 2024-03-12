79a12552f99e278c0fd6dbd785b5757112234d08
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-03-12 11:15:59 -0700

    console.py

--- console.py
+++ console.py
@@ -135,7 +135,7 @@ if __name__ == "__main__":
 
     import sys
 
-    if len(sys.argv) > 1:
-        HBNBCommand().onecmd(' '.join(sys.argv[1:]))
-    else:
-        HBNBCommand().cmdloop()
+    # if len(sys.argv) > 1:
+    #   HBNBCommand().onecmd(' '.join(sys.argv[1:]))
+    # else:
+    HBNBCommand().cmdloop()
---
+++ models/user.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module contains User class"""
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """User inherits from BaseModel"""
+
